e73838b2b0d132bc14f858a3fa3968cd5e38cc2e
Eddy <eddy.li@pentium.network>
2018-08-15 17:49:11 +0800

    VerifyCsrfToken

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,8 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        '/post/add',
+        '/post/update/{uuid}',
+        '/post/delete'
     ];
 }
